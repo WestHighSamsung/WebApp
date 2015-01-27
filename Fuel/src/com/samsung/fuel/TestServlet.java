7f442e4cbe5b31a844a412733b4c2d812c8a5a70
MadcowD <carlostnb@gmail.com>
2015-01-26 23:39:30 -0700

    Changed servlet code

--- Fuel/src/com/samsung/fuel/TestServlet.java
+++ Fuel/src/com/samsung/fuel/TestServlet.java
@@ -1,7 +1,18 @@
 package com.samsung.fuel;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
+
+class tits{
+	String name;
+	String email;
+}
 
 @SuppressWarnings("serial")
 public class TestServlet extends HttpServlet {
@@ -9,5 +20,13 @@ public class TestServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		tits b = new tits();
+		
+		
+		Gson gson = new Gson();
+		gson.toJson(b);
+		resp.getWriter().println(gson.toJson(b));
+		String name = req.getParameter("NAME");
+		resp.getWriter().println(name);
 	}
 }
