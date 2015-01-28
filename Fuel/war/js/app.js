16b698d5aab237187097b6a6b5b39992c50e1d58
MadcowD <carlostnb@gmail.com>
2015-01-27 23:01:04 -0700

    Converted Maps into Ember

--- Fuel/war/js/app.js
+++ Fuel/war/js/app.js
@@ -1,1 +1,33 @@
 App = Ember.Application.create();
+App.GoogleMapsComponent  = Ember.Component.extend({
+	insertMap: function() {
+		var directionsService = new google.maps.DirectionsService();
+    	var map;
+    	var westhigh = new google.maps.LatLng(40.774534, -111.900473);
+    	var testLoc = new google.maps.LatLng(40.773055, -111.882215);
+		var container = this.$(".map-canvas");
+		directionsDisplay = new google.maps.DirectionsRenderer();
+        var mapOptions = {
+          zoom: 14,
+          center: westhigh
+        }
+        map = new google.maps.Map(container[0], mapOptions);
+        var defaultBounds = new google.maps.LatLngBounds(
+          westhigh,
+          testLoc);
+        map.fitBounds(defaultBounds);
+        var input =($('#searchbar')[0]);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var searchBox = new google.maps.places.Autocomplete(input);
+
+         google.maps.event.addListener(searchBox, 'place_changed', function() {
+          var place = searchBox.getPlace();
+          calcRoute(place.geometry.location, "DRIVING", directionsService);
+          calcRoute(place.geometry.location, "BICYCLING", directionsService);
+        });
+        google.maps.event.addListener(map, 'bounds_changed', function() {
+          var bounds = map.getBounds();
+          searchBox.setBounds(bounds);
+        });
+	}
+})
--- Fuel/war/js/mapsConnector.js
+++ Fuel/war/js/mapsConnector.js
@@ -2,32 +2,7 @@
       var map;
       var westhigh = new google.maps.LatLng(40.774534, -111.900473);
       var testLoc = new google.maps.LatLng(40.773055, -111.882215);
-      function initialize() {
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        var mapOptions = {
-          zoom: 14,
-          center: westhigh
-        }
-        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        var defaultBounds = new google.maps.LatLngBounds(
-          westhigh,
-          testLoc);
-        map.fitBounds(defaultBounds);
-        var input =($('#searchbar')[0]);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        var searchBox = new google.maps.places.Autocomplete(input);
-
-         google.maps.event.addListener(searchBox, 'place_changed', function() {
-          var place = searchBox.getPlace();
-          calcRoute(place.geometry.location, "DRIVING");
-          calcRoute(place.geometry.location, "BICYCLING");
-        });
-        google.maps.event.addListener(map, 'bounds_changed', function() {
-          var bounds = map.getBounds();
-          searchBox.setBounds(bounds);
-        });
-      }
-      function calcRoute(place, transType) {
+      function calcRoute(place, transType, directionsServ) {
       	var directionsDisplay = new google.maps.DirectionsRenderer();
       	directionsDisplay.setMap(map);
         var request = {
@@ -35,7 +10,7 @@
             destination: westhigh,
             travelMode: google.maps.TravelMode[transType]
         };
-        directionsService.route(request, function(response, status) {
+        directionsServ.route(request, function(response, status) {
         	if(transType === "DRIVING") {
         		console.log("Carbon Emissions of Driving");
         		console.log(response.routes[0].legs[0].distance.value/1609.34*430);
@@ -47,5 +22,5 @@
           }
         });
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
+      // google.maps.event.addDomListener(window, 'load', initialize);
     
