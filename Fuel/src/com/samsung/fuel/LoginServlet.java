23bd6f386f76cb461ee05acb6a72722303cffa36
Patrick Bo Chen <patrickbochen@gmail.com>
2015-01-27 21:59:49 -0700

    Debuged the index/login page
    Fixed key creation issue that was creating errors

--- Fuel/src/com/samsung/fuel/LoginServlet.java
+++ Fuel/src/com/samsung/fuel/LoginServlet.java
@@ -1,21 +1,14 @@
-package com.samsung.conserve;
+package com.samsung.fuel;
 import java.io.IOException;
 
-import javax.servlet.http.*;
-
+import javax.servlet.ServletException;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-
-import java.io.IOException;
-import java.util.Date;
-
+import com.google.gson.Gson;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,49 +17,57 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet 
 {	
-	//I THINK WE NEED TO PASS IN A DATASTORE OBJECT
-	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException 
 	{
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
-		//UserService userService = UserServiceFactory.getUserService();
-		//User user = userService.getCurrentUser();
 
 		//Initializes variables and recieves data from the request
 		String enType = "User";
-		String accTok = req.getParameter("guestbookName");
-		String userID = req.getParameter("guestbookName");
-		String address = null, name= null, email= null, co2= null, 
-				travelType= null, route = null;
+		String accTok = req.getParameter("accTok");
+		String userID = req.getParameter("userID");
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String address = null, co2= null, travelType= null, route = null;
 		boolean isCarpool = false;
+
+		//Create key for entity object using type of entity and accountToken
+		Key k = KeyFactory.createKey(enType, accTok);
 		
-		//Create a key to store the datapoint
-		Key accTokKey = KeyFactory.createKey(enType, accTok);
-		
-		//Check if user data exists
-		try
+		//Check if entity already exists in database
+		try{
+			datastore.get(k);
+		}
+		catch(EntityNotFoundException e)
 		{
-			datastore.get(accTokKey);
-		}catch(EntityNotFoundException e)
-		{
-			//Create new user
-			Entity user = new Entity(enType, accTokKey);
+			//Insert new user and properties into database
+			Entity user = new Entity(enType, accTok);
+			user.setProperty("accTok", accTok);
 			user.setProperty("userID", userID);
-			user.setProperty("address", null);
-			user.setProperty("name", null);
-			user.setProperty("email", null);
-			user.setProperty("co2", null);
-			user.setProperty("travelType", null);
-			user.setProperty("route", null);
+			user.setProperty("name", name);
+			user.setProperty("email", email);
 			user.setProperty("isCarpool", isCarpool);
+			user.setProperty("address", address);
+			user.setProperty("co2", co2);
+			user.setProperty("travelType", travelType);
+			user.setProperty("route", route);
 			
 			datastore.put(user);
 		}
 
-		resp.sendRedirect("index.html/update");
+		//Returns json output of object
+		Gson translate = new Gson();
+		try {
+			resp.getWriter().println(translate.toJson(datastore.get(k)));
+		} catch (EntityNotFoundException e) {
+			resp.getWriter().println("sdsd");
+		}
+	}
 
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException,
+			ServletException {
+		doPost(request, response);
 	}
 }
