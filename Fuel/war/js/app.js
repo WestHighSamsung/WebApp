67c68d983e5921dbba48751a00166aff629c380e
Phillip Kuznetsov <superpker@gmail.com>
2015-01-28 09:08:25 -0700

    Trying to output JSON from calcRoute() which will be displayed in the table. I broke the webpage though, so if you want to run, go to mapsConnector.js

--- Fuel/war/js/app.js
+++ Fuel/war/js/app.js
@@ -29,32 +29,32 @@ App.GoogleMapsComponent = Ember.Component.extend({
 	insertMap: function() {
 		var container = this.$(".map-canvas");
 		var directionsService = new google.maps.DirectionsService();
-    	var map;
-    	var westhigh = new google.maps.LatLng(40.774534, -111.900473);
-    	var testLoc = new google.maps.LatLng(40.773055, -111.882215);
+  	var map;
+  	var westhigh = new google.maps.LatLng(40.774534, -111.900473);
+  	var testLoc = new google.maps.LatLng(40.773055, -111.882215);
 		
 		directionsDisplay = new google.maps.DirectionsRenderer();
-        var mapOptions = {
-          zoom: 14,
-          center: westhigh
-        }
-        map = new google.maps.Map(container[0], mapOptions);
-        var defaultBounds = new google.maps.LatLngBounds(
-          westhigh,
-          testLoc);
-        map.fitBounds(defaultBounds);
-        var input =($('#searchbar')[0]);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        var searchBox = new google.maps.places.Autocomplete(input);
+    var mapOptions = {
+      zoom: 14,
+      center: westhigh
+    }
+    map = new google.maps.Map(container[0], mapOptions);
+    var defaultBounds = new google.maps.LatLngBounds(
+      westhigh,
+      testLoc);
+    map.fitBounds(defaultBounds);
+    var input =($('#searchbar')[0]);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.Autocomplete(input);
 
-        google.maps.event.addListener(searchBox, 'place_changed', function() {
-          var place = searchBox.getPlace();
-          calcRoute(place.geometry.location, "DRIVING", directionsService, map);
-          calcRoute(place.geometry.location, "BICYCLING", directionsService, map);
-        });
-        google.maps.event.addListener(map, 'bounds_changed', function() {
-          var bounds = map.getBounds();
-          searchBox.setBounds(bounds);
-        });
-	 }.on('didInsertElement')
+    google.maps.event.addListener(searchBox, 'place_changed', function() {
+      var place = searchBox.getPlace();
+      console.log(calcRoute(place.geometry.location, "DRIVING", directionsService, map));
+      console.(calcRoute(place.geometry.location, "BICYCLING", directionsService, map));
+    });
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });
+	}.on('didInsertElement')
 });
--- Fuel/war/js/mapsConnector.js
+++ Fuel/war/js/mapsConnector.js
@@ -1,5 +1,8 @@
       var westhigh = new google.maps.LatLng(40.774534, -111.900473);
       function calcRoute(place, transType, directionsServ, map) {
+        var carbon;
+        var dist;
+        var time;
       	var directionsDisplay = new google.maps.DirectionsRenderer();
       	directionsDisplay.setMap(map);
         var request = {
@@ -8,16 +11,27 @@
             travelMode: google.maps.TravelMode[transType]
         };
         directionsServ.route(request, function(response, status) {
+          dist = response.routes[0].legs[0].distance.value;
+          time = response.routes[0].legs[0].duration.value;
         	if(transType === "DRIVING") {
-        		console.log("Carbon Emissions of Driving");
-        		console.log(response.routes[0].legs[0].distance.value/1609.34*430);
+            carbon = distance*1609.34*430;
+        	} else if(transType === "TRANSIT") {
+            carbon = distance*10;//this is currently a wrong value; fix it
+          } else {
+            carbon = 0;
+          }
 
-        	}
-          
+          console.log(distance + " "+ time + " "+ carbon);
+          var retJSON = {
+            distance = dist,  
+            duration = time, 
+            emissions = carbon
+          };
           if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
           }
         });
+        return retJSON;
       }
       // google.maps.event.addDomListener(window, 'load', initialize);
     
