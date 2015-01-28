c9dc2eaed39ceecc7ad7ac6e913c158b70bdc8e2
MadcowD <carlostnb@gmail.com>
2015-01-28 07:25:03 -0700

    YESA

--- Fuel/war/js/app.js
+++ Fuel/war/js/app.js
@@ -9,14 +9,30 @@ App.Router.map(function(){
 	this.resource('about')
 });
 
+// App.GoogleMapsComponent = Ember.Component.extend({
+//   insertMap: function() {
+//     var container = this.$(".map-canvas");
 
-App.GoogleMapsComponent  = Ember.Component.extend({
+//     var options = {
+//       center: new google.maps.LatLng(this.get("latitude"),
+// this.get("longitude")),
+//       zoom: 17,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+
+//     new google.maps.Map(container[0], options);
+//   }.on('didInsertElement')
+// });
+
+
+App.GoogleMapsComponent = Ember.Component.extend({
 	insertMap: function() {
+		var container = this.$(".map-canvas");
 		var directionsService = new google.maps.DirectionsService();
     	var map;
     	var westhigh = new google.maps.LatLng(40.774534, -111.900473);
     	var testLoc = new google.maps.LatLng(40.773055, -111.882215);
-		var container = this.$(".map-canvas");
+		
 		directionsDisplay = new google.maps.DirectionsRenderer();
         var mapOptions = {
           zoom: 14,
@@ -31,14 +47,14 @@ App.GoogleMapsComponent  = Ember.Component.extend({
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
         var searchBox = new google.maps.places.Autocomplete(input);
 
-         google.maps.event.addListener(searchBox, 'place_changed', function() {
+        google.maps.event.addListener(searchBox, 'place_changed', function() {
           var place = searchBox.getPlace();
-          calcRoute(place.geometry.location, "DRIVING", directionsService);
-          calcRoute(place.geometry.location, "BICYCLING", directionsService);
+          calcRoute(place.geometry.location, "DRIVING", directionsService, map);
+          calcRoute(place.geometry.location, "BICYCLING", directionsService, map);
         });
         google.maps.event.addListener(map, 'bounds_changed', function() {
           var bounds = map.getBounds();
           searchBox.setBounds(bounds);
         });
-	}
-})
+	 }.on('didInsertElement')
+});
--- Fuel/war/js/mapsConnector.js
+++ Fuel/war/js/mapsConnector.js
@@ -1,8 +1,5 @@
-      var directionsService = new google.maps.DirectionsService();
-      var map;
       var westhigh = new google.maps.LatLng(40.774534, -111.900473);
-      var testLoc = new google.maps.LatLng(40.773055, -111.882215);
-      function calcRoute(place, transType, directionsServ) {
+      function calcRoute(place, transType, directionsServ, map) {
       	var directionsDisplay = new google.maps.DirectionsRenderer();
       	directionsDisplay.setMap(map);
         var request = {
